2266000815a3230c5b66e70cd969ea6ad1a08807
fraenk <gitmail@frankwalter.net>
2015-02-19 12:06:06 +0100

    Revert "remove workaround for #730"
    This reverts commit e12a4b729aedfabae3728ec73e873c5fbd5f6c25.

--- library/bones.php
+++ library/bones.php
@@ -269,7 +269,8 @@ function bones_page_navi() {
     'next_text'    => '&rarr;',
     'type'         => 'list',
     'end_size'     => 3,
-    'mid_size'     => 3
+    'mid_size'     => 3,
+    'add_args'     => false
   ) );
   echo '</nav>';
 } /* end page navi */
